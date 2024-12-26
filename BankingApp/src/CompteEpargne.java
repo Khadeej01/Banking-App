00e0b91fa9397f8af52d5482b4b5bcb943b64cfb
khadijaDev <khadijaoukhlift@gmail.com>
2024-12-26 17:08:51 +0100

    Gestion du compteEpargne

--- BankingApp/src/CompteEpargne.java
+++ BankingApp/src/CompteEpargne.java
@@ -1,14 +1,20 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class CompteEpargne extends Compte {
-    private double  tauxInteret;
+    private double tauxInteret;
 
 
-
-    public CompteEpargne(double tauxInteret, String numero, double solde, Client client){
-        super(numero,solde,client);
-        this.tauxInteret=tauxInteret;
+    public CompteEpargne(double tauxInteret, String numero, double solde, Client client) {
+        super(numero, solde, client);
+        this.tauxInteret = tauxInteret;
 
 
     }
+    public CompteEpargne() {
+        
+    }
+
     public double getTauxInteret() {
         return tauxInteret;
     }
@@ -17,12 +23,23 @@ public class CompteEpargne extends Compte {
         this.tauxInteret = tauxInteret;
     }
 
-
-
-    @Override
-    public String toString() {
-        return "CompteEpargne{" +
-                "tauxInteret=" + tauxInteret +
-                "} " + super.toString();
-    }
-}
+    //Gestion du compte epargne
+    
+    public  void GestionCompteEpargne(ArrayList<CompteEpargne> compteEpargnes,ArrayList<Client> clients ) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ajouter un compte Epargnes ");
+        System.out.println("Afficher un compte Epargnes ");
+        System.out.println("Tapez votre choix");
+        int choix = sc.nextInt();
+        switch (choix) {
+            case 1:
+                AjouterCompteEpargne(compteEpargnes,clients);
+                break;
+            case 2:
+                AfficherCompteEpargne(compteEpargnes, clients);
+                break;
+            default:
+                System.out.println("Choix invalide");
+                break;
+        }
+    }}
