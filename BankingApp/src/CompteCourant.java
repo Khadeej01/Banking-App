a7389e11257d1058605d6de795495f9009bad70d
khadijaDev <khadijaoukhlift@gmail.com>
2024-12-26 17:03:20 +0100

    Gestion du CompteCourant

--- BankingApp/src/CompteCourant.java
+++ BankingApp/src/CompteCourant.java
@@ -1,25 +1,47 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class CompteCourant extends Compte {
-    private String FraisBancaire;
-    public  CompteCourant(String FraisBancaire,String numero,double solde,Client client ) {
-        super( numero , solde, client);
+    private double FraisBancaire;
+
+    ArrayList<CompteCourant> compteCourants = new ArrayList<CompteCourant>();
+    Scanner sc = new Scanner(System.in);
+
+    public CompteCourant(double FraisBancaire, String numero, double solde, Client client) {
+        super(numero, solde, client);
         this.FraisBancaire = FraisBancaire;
 
 
     }
+    public CompteCourant() {
 
-    public String getFraisBancaire() {
+    }
+
+    public double getFraisBancaire() {
         return FraisBancaire;
     }
 
-    public void setFraisBancaire(String FraisBancaire) {
-       this.FraisBancaire = FraisBancaire;
+    public void setFraisBancaire(double FraisBancaire) {
+        this.FraisBancaire = FraisBancaire;
     }
 
-    @Override
-    public String toString() {
-        return "CompteCourant{" +
-                "FraisBancaire='" + FraisBancaire + '\'' +
-                "} " + super.toString();
+    // Gestion du compte Courant
+    public void GestionCompteCourant(ArrayList<CompteCourant> compteCourants,ArrayList<Client> clients) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ajouter un compte courant ");
+        System.out.println("Afficher un compte courant ");
+        System.out.println("Tapez votre choix");
+        int choix = sc.nextInt();
+        switch (choix) {
+            case 1:
+                AjouterCompteCourant(compteCourants,clients);
+                break;
+            case 2:
+                AfficherCompteCourant(compteCourants);
+                break;
+            default:
+                System.out.println("Choix invalide");
+                break;
+        }
     }
 }
-
